3be971bcce6696d38ef0e567d9e4a19c38ee3e59
villa_mou <mouxuefei@edocyun.com.cn>
2022-09-26 14:01:25 +0800

    chore: header

--- app/src/main/java/com/edocyun/timchat/vp/api/ChatRetrofit.kt
+++ app/src/main/java/com/edocyun/timchat/vp/api/ChatRetrofit.kt
@@ -1,5 +1,7 @@
 package com.edocyun.timchat.vp.api
 
+import android.os.Build
+import com.edocyun.timchat.TUIConfig
 import com.edocyun.timchat.network.constant.ApiConfig
 import com.edocyun.timchat.network.retrofit.RetrofitFactory
 import okhttp3.Request
@@ -13,22 +15,28 @@ import okhttp3.Request
  */
 
 object ChatRetrofit : RetrofitFactory<ChatApi>() {
+
     override fun getBaseUrl() = ApiConfig.domain + ApiConfig.basePath + ApiConfig.apiVersion
+
     override fun getHeader(builder: Request.Builder): Request.Builder {
-        builder.addHeader("token", "XXXXXXXXXXXXXXXXXXXXX")
-        builder.addHeader("Content-Type", "application/json")
-        /**
-        const headers = {
-        'Device-Brand': deviceInfo.brand,
-        'Device-Code': deviceInfo.uniqueId,
-        'Device-Model': deviceInfo.model,
-        'Device-Name': deviceInfo.name,
-        'Device-Type': Platform.select({ ios: 'iOS', default: 'Android' }),
-        'OS-Version': deviceInfo.osVersion,
-        Version: deviceInfo.appVersion,
-        Authorization: reqToken,
-        };
-         */
+        var appName: String?
+        var appVersion: String?
+        try {
+            appVersion = TUIConfig.getAppContext().packageManager.getPackageInfo(TUIConfig.getAppContext().packageName, 0).versionName
+            appName = TUIConfig.getAppContext().applicationInfo
+                .loadLabel(TUIConfig.getAppContext().packageManager).toString()
+        } catch (e: Exception) {
+            appVersion = "unknown"
+            appName = "unknown"
+        }
+        builder.addHeader("Authorization", "XXXXXXXXXXXXXXXXXXXXX")
+        builder.addHeader("Version", appVersion?:"")
+        builder.addHeader("OS-Version", Build.VERSION.RELEASE)
+        builder.addHeader("Device-Type", "Android")
+        builder.addHeader("Device-Name", appName?:"云朵医生")
+        builder.addHeader("Device-Model", Build.MODEL)
+        builder.addHeader("Device-Code", "xxxxx")
+        builder.addHeader("Device-Brand", Build.BRAND)
         return builder
     }
 
